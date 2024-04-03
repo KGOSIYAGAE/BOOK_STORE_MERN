a0565609725d13a5d70cee211f4aad9a6552f86c
Kgosiyagae Motabogi <ndosisetlole@gmail.com>
2024-04-03 20:45:58 +0200

    Created the route for getting all books, book by id and update book

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,2 @@
 export const PORT = 3000;
-export const mongoDBURL = `mongodb+srv://root:kgosi123@book-store-mern.jpzxjfi.mongodb.net/?retryWrites=true&w=majority&appName=Book-Store-MERN`;
+export const mongoDBURL = `mongodb+srv://root:kgosi123@book-store-mern.jpzxjfi.mongodb.net/books_collection?retryWrites=true&w=majority&appName=Book-Store-MERN`;
--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,57 @@ app.post("/books", async (req, res) => {
     return res.status(201).send(book);
   } catch (error) {
     console.log(error.message);
-    res.status(500).send({ message: error.message });
+    res.status(501).send({ message: error.message });
+  }
+});
+
+//Route for getting all books
+app.get("/books", async (req, res) => {
+  try {
+    const books = await Book.find({});
+
+    return res.status(200).json({
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(501).send({ message: error.message });
+  }
+});
+
+//Route for getting a book by id
+app.get("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const book = await Book.findById(id);
+
+    return res.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    res.status(501).send({ message: error.message });
+  }
+});
+
+//Route for updating a book
+app.put("/books/:id", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({ message: "Send all required fields: title, author and publishYear" });
+    }
+
+    const { id } = req.params;
+    const result = await Book.findByIdAndUpdate(id, req.body);
+
+    if (!result) {
+      return res.status(400).send({ message: "Book not found" });
+    }
+
+    return res.status(200).send({ message: "Book successfully updated" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(501).send({ message: error.message });
   }
 });
 
