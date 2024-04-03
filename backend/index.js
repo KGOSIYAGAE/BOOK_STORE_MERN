050655e6b72941942edb117d8acc9be8e7a41e1e
Kgosiyagae Motabogi <ndosisetlole@gmail.com>
2024-04-03 18:59:09 +0200

    Created the route for adding new book

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,26 @@ import { Book } from "./models/BookModel.js";
 
 const app = express();
 
+//Middleware for passing request body
+app.use(express.json());
+
 app.get("/", (req, res) => {});
 
-//Route to get all books
-
-app.get("/books", async (req, res) => {
+//Route to create new book
+app.post("/books", async (req, res) => {
   try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({ message: "Send all required fields: title, author, publishYear" });
+    }
+
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+    return res.status(201).send(book);
   } catch (error) {
     console.log(error.message);
     res.status(500).send({ message: error.message });
