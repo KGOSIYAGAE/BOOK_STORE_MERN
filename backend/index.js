119183e22da0f69429c5c18cf414e9429d3c92ae
Kgosiyagae Motabogi <ndosisetlole@gmail.com>
2024-04-04 21:58:46 +0200

    Refactored server code, added middleware to handle cors policy, initialized frontend with vite

--- backend/index.js
+++ backend/index.js
@@ -2,84 +2,26 @@ import express from "express";
 import { mongoDBURL, PORT } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/BookModel.js";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 
 //Middleware for passing request body
 app.use(express.json());
 
+//Middleware for handling CORS Policy
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+
 app.get("/", (req, res) => {});
 
-//Route to create new book
-app.post("/books", async (req, res) => {
-  try {
-    if (!req.body.title || !req.body.author || !req.body.publishYear) {
-      return res.status(400).send({ message: "Send all required fields: title, author, publishYear" });
-    }
-
-    const newBook = {
-      title: req.body.title,
-      author: req.body.author,
-      publishYear: req.body.publishYear,
-    };
-
-    const book = await Book.create(newBook);
-    return res.status(201).send(book);
-  } catch (error) {
-    console.log(error.message);
-    res.status(501).send({ message: error.message });
-  }
-});
-
-//Route for getting all books
-app.get("/books", async (req, res) => {
-  try {
-    const books = await Book.find({});
-
-    return res.status(200).json({
-      count: books.length,
-      data: books,
-    });
-  } catch (error) {
-    console.log(error.message);
-    res.status(501).send({ message: error.message });
-  }
-});
-
-//Route for getting a book by id
-app.get("/books/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    const book = await Book.findById(id);
-
-    return res.status(200).json(book);
-  } catch (error) {
-    console.log(error.message);
-    res.status(501).send({ message: error.message });
-  }
-});
-
-//Route for updating a book
-app.put("/books/:id", async (req, res) => {
-  try {
-    if (!req.body.title || !req.body.author || !req.body.publishYear) {
-      return res.status(400).send({ message: "Send all required fields: title, author and publishYear" });
-    }
-
-    const { id } = req.params;
-    const result = await Book.findByIdAndUpdate(id, req.body);
-
-    if (!result) {
-      return res.status(400).send({ message: "Book not found" });
-    }
-
-    return res.status(200).send({ message: "Book successfully updated" });
-  } catch (error) {
-    console.log(error.message);
-    res.status(501).send({ message: error.message });
-  }
-});
+app.use("/books", booksRoute);
 
 mongoose
   .connect(mongoDBURL)
