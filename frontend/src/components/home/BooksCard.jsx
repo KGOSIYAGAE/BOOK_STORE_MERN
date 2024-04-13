5d4085bd092c61dec2727337b129c2b3557a02ec
Kgosiyagae Motabogi <ndosisetlole@gmail.com>
2024-04-13 20:05:13 +0200

    Wrote code to display books in card view on button click

--- frontend/src/components/home/BooksCard.jsx
+++ frontend/src/components/home/BooksCard.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
-export default function BooksCard() {
-  return <div>BooksCard</div>;
+import BooksSingleCard from "./BooksSingleCard";
+
+export default function BooksCard({ books }) {
+  return (
+    <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {books.map((item) => (
+        <BooksSingleCard key={item.id} book={item} />
+      ))}
+    </div>
+  );
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,8 +3,6 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import Spinner from "../components/Spinner";
 import { Link } from "react-router-dom";
-import { AiOutlineEdit } from "react-icons/ai";
-import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
 import BooksTable from "../components/home/BooksTable";
 import BooksCard from "../components/home/BooksCard";
