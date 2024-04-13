dbc73ac54a81f9e42e06b4ec8c43d425eb6d3504
Kgosiyagae Motabogi <ndosisetlole@gmail.com>
2024-04-13 15:37:52 +0200

    Wrote code to update and delete a book

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -1,5 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
+import BackButton from "../components/BackButton";
+import axios from "axios";
 
 export default function DeleteBook() {
-  return <div>DeleteBook</div>;
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [loading, setLoading] = useState(false);
+
+  const handleDeleteBook = () => {
+    setLoading(true);
+    axios
+      .delete(`http://localhost:3000/books/${id}`)
+      .then(() => {
+        setLoading(false);
+        navigate("/");
+      })
+      .catch((error) => {
+        alert("An error occured, please check console");
+        console.log(error);
+        setLoading(false);
+      });
+  };
+  return (
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3x1 my-4">Delete Book</h1>
+      {loading ? <Spinner /> : ""}
+      <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] px-8 mx-auto">
+        <h3 className="text-2xl">Are You Sure You want to delete this book?</h3>
+        <button className="p-4 bg-red-600 text-white m-8 w-full" onClick={handleDeleteBook}>
+          Yes, Delete it
+        </button>
+      </div>
+    </div>
+  );
 }
--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -1,5 +1,75 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Spinner from "../components/Spinner";
+import BackButton from "../components/BackButton";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
 
 export default function EditBook() {
-  return <div>EditBook</div>;
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [publishYear, setPublishYear] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`http://localhost:3000/books/${id}`)
+      .then((response) => {
+        setTitle(response.data.title);
+        setAuthor(response.data.author);
+        setPublishYear(response.data.publishYear);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+
+  const handleEditBook = () => {
+    const data = {
+      title,
+      author,
+      publishYear,
+    };
+    setLoading(true);
+    axios
+      .put(`http://localhost:3000/books/${id}`, data)
+      .then(() => {
+        setLoading(false);
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("An error happened. Please check console");
+        setLoading(false);
+      });
+  };
+  return (
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4">Edit Book</h1>
+      {loading ? <Spinner /> : ""}
+      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+        <div className="flex flex-col my-4">
+          <label className="text-xl mr-4 text-gray-500">Title</label>
+          <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} className="border-2 border-gray-500 px-4"></input>
+        </div>
+        <div className="flex flex-col my-4">
+          <label className="text-xl mr-4 text-gray-500">Author</label>
+          <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} className="border-2 border-gray-500 px-4"></input>
+        </div>
+        <div className="flex flex-col my-4">
+          <label className="text-xl mr-4 text-gray-500">Year Published</label>
+          <input type="text" value={publishYear} onChange={(e) => setPublishYear(e.target.value)} className="border-2 border-gray-500 px-4"></input>
+        </div>
+        <button className="p-2 bg-sky-300 m-8" onClick={handleEditBook}>
+          Save
+        </button>
+      </div>
+    </div>
+  );
 }
